659bc3bf104d71165f1778a946a7df9d675c54ea
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-04 19:19:51 -0400

    Update test_helper.js

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test',{
+mongoose.connect('mongodb://localhost/books_test',{
     useMongoClient: true,   
 });
 mongoose.connection
-    .once('open',() => console.log("Bravo t'es chaud comme une baraque à frite ! Connexion établie"))
+    .once('open',() => console.log("Yeah ! Connexion établie"))
     .on('error',(error) => {
         console.warn('Warning',error);
     });
