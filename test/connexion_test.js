b3e985144fcbae4cb3759399962b82607e6a3bd9
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-07 19:52:16 -0400

    rename

---
+++ test/connexion_test.js
@@ -0,0 +1,11 @@
+// librairie pour discuter avec mongodb en js
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/books_test',{
+    useMongoClient: true,   
+});
+mongoose.connection
+    .once('open',() => console.log("Bravo ! Connexion établie"))
+    .on('error',(error) => {
+        console.warn('Warning',error);
+    });
