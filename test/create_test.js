acc3d69d466abfd2512e4585a56ef38f04b4180b
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-07 20:45:23 -0400

    Premiers Hooks

--- test/create_test.js
+++ test/create_test.js
@@ -2,9 +2,13 @@ const assert = require('assert');
 const Book = require('../src/books');
 
 describe('Creation de livre', () => {
-    it('Sauvegarde un livre', () => {
+    it("Sauvegarde un livre et s'assure qu il n'est pas deja en base", (done) => {
             const book1 = new Book({title:"Harry Potter"});
-            book1.save();
+            book1.save( () => {
+                assert(!book1.isNew);
+                done();
+            });
+            
 
     })
 })
