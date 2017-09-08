74051a19f4e4643464b9dda4cc0358e0d755ebe1
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-07 20:58:33 -0400

    Update connexion_test.js

--- test/connexion_test.js
+++ test/connexion_test.js
@@ -1,10 +1,11 @@
+// librairie pour discuter avec mongodb en js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test',{
+mongoose.connect('mongodb://localhost/books_test',{
     useMongoClient: true,   
 });
 mongoose.connection
-    .once('open',() => console.log("Bravo tu es connecté ! Connexion établie"))
+    .once('open',() => console.log("Bravo ! Connexion établie"))
     .on('error',(error) => {
-        console.warn('Warning',error);
+        console.warn('Erreurs : ',error);
     });
