f913ae0828f502599531a9d96286be6798d6872f
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-08 21:23:24 -0400

    find

--- test/read_test.js
+++ test/read_test.js
@@ -10,14 +10,22 @@ describe('Test du read', () => {
         });  
     })
 
-    it("Recherche du livre Harry Potter et test qu'il à le meme id que celui inséré avant", (done) => {
+    it("Recherche du livre Harry Potter et test qu'il a le meme id que celui inséré avant", (done) => {
         //Recherche tous les livres dont le title est Harry Potter
         Book.find({ title: 'Harry Potter'}).then((books) => {
             // Attention dans mongoDB les id sont des objets et le === ou ==  comparerait donc les adresses !
-            // Utilisez la fonction equals qui elle compare les string des id
+            // Utilisez la fonction equals qui, elle, compare les string des id
             assert(book1._id.equals(books[0]._id)); 
             done();
         });
-    });          
+    });
+    
+    it('Reherche un livre par son id', (done) => {
+        Book.findOne({ _id: book1._id })
+        .then((book) => {
+            assert(book.title === 'Harry Potter');
+            done();
+        });
+    });
 });
   
