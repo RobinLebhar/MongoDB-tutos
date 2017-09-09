6c48719e820ff7d71d86f287cd11ab451df590bf
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-09 17:26:21 -0400

    relations

--- test/connexion_test.js
+++ test/connexion_test.js
@@ -21,7 +21,12 @@ before((done) => {
 beforeEach('Supprime les anciens livres avant chaque tests' ,(done) => {
     const { books } = mongoose.connection.collections;
     books.drop( () => {
+       const { users } = mongoose.connection.collections;
+       users.drop( () => {
         done();
     });
+    });
+     
+    
 })
 
